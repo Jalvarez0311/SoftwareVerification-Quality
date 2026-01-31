0a4169720a053fefb6af9266602965e86e103a54
Jorgeandres Alvarez <96158104+Jalvarez0311@users.noreply.github.com>
2026-01-31 12:06:50 -0500

    Create MinTest class for unit testing
    Add MinTest class with setup and teardown methods

---
+++ Classwork/testMinDemo/src/MinTest.java
@@ -0,0 +1,11 @@
+package Classwork.testMinDemo.src;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MinTest {
+    @BeforeEach
+    void setup(){
+        list = new ArrayString<String>();
+    }
+
+    @AfterEach
+}
