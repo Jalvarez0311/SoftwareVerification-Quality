b62394fca40a2098ff18a2a3952bd4d9e760c8ab
Jorgeandres Alvarez <96158104+Jalvarez0311@users.noreply.github.com>
2026-01-31 12:09:17 -0500

    Create Min class for finding minimum values
    Implement a class to find the minimum value in a list of integers with appropriate error handling.

---
+++ Classwork/testMinDemo/Test/Min.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Min {
+    /**
+    * Returns the minimum value from a list of integers.
+    * @param list Comparable list of elements to search
+    * @return The minimum element in the list.
+    * @throws NullPointerException if the list/elements are null.
+    * @throws ClassCastException if the elements are not comparable.
+    * @throws IllegalArgumentException if the list is empty.
+     */
+
+    public static <T extends Comparable<?
+}
